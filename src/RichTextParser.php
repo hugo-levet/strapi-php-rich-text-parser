50c49a9d22b654b101dc473273a195df77713a60
Hugo Levet <hugolevet.pro@gmail.com>
2024-08-20 23:52:03 +0200

    Add code and tests

---
+++ src/RichTextParser.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace HugoLevet\StrapiPhpRichTextParser;
+
+class RichTextParser
+{
+    public static function parseText($data): string
+    {
+        $html_content = '';
+
+        if (!empty($data->bold) && $data->bold === true) {
+            $html_content .= '<strong>' . htmlspecialchars($data->text) . '</strong>';
+        } elseif (!empty($data->italic) && $data->italic === true) {
+            $html_content .= '<em>' . htmlspecialchars($data->text) . '</em>';
+        } elseif (!empty($data->underline) && $data->underline === true) {
+            $html_content .= '<u>' . htmlspecialchars($data->text) . '</u>';
+        } elseif (!empty($data->strikethrough) && $data->strikethrough === true) {
+            $html_content .= '<del>' . htmlspecialchars($data->text) . '</del>';
+        } elseif (!empty($data->code) && $data->code === true) {
+            $html_content .= '<code>' . htmlspecialchars($data->text) . '</code>';
+        } else {
+            $html_content .= htmlspecialchars($data->text);
+        }
+
+        return $html_content;
+    }
+
+    public static function parseList($value)
+    {
+        $html_content = '';
+        $item_tag = $value->format == 'unordered' ? 'ul' : 'ol';
+        $html_content .= '<' . $item_tag . '>';
+        foreach ($value->children as $key => $child) {
+            if ($child->type == 'list-item') {
+                $html_content .= '<li>';
+                foreach ($child->children as $key => $subchild) {
+                    $html_content .= RichTextParser::parseBlockText($subchild);
+                }
+                $html_content .= '</li>';
+            } elseif ($child->type == 'list') {
+                $html_content .= '<li>' . RichTextParser::parseList($child) . '</li>';
+            } else {
+                $html_content .= '<!-- ' . $child->type . ' is not implemented yet -->';
+                // not implemented
+            }
+        }
+        $html_content .= '</' . $item_tag . '>';
+        return $html_content;
+    }
+
+    public static function parseBlockText($data): string
+    {
+        $html_content = '';
+        $type = $data->type;
+
+        if ($type == 'text') {
+            $html_content .= RichTextParser::parseText($data);
+        } elseif ($type == 'link') {
+            $is_external_link = $data->url[0] !== '/';
+            $html_content .=
+                '<a href="' .
+                $data->url .
+                '" ' .
+                ($is_external_link ? 'target="_blank" rel="noopener noreferrer"' : '') .
+                '>' .
+                RichTextParser::parseText($data->children[0]) .
+                '</a>';
+        } else {
+            $html_content .= '<!-- ' . $data->type . ' is not implemented yet -->';
+            // not implemented
+        }
+
+        return $html_content;
+    }
+    public
+
+    static function jsonToHtml($json): string
+    {
+        $html_content = '';
+        foreach ($json as $key => $value) {
+            switch ($value->type) {
+                case 'paragraph':
+                    $html_content .= '<p>';
+                    foreach ($value->children as $key => $child) {
+                        $html_content .= RichTextParser::parseBlockText($child);
+                    }
+                    $html_content .= '</p>';
+                    break;
+
+                case 'image':
+                    $image = $value->image->formats->medium;
+                    $html_content .=
+                        '<img src="' .
+                        $_ENV["STRAPI_URL"] .
+                        $image->url .
+                        '" alt="' .
+                        $value->image->alternativeText .
+                        '" width="' .
+                        $image->width .
+                        '" height="' .
+                        $image->height .
+                        '" loading="lazy" />';
+                    break;
+
+                case 'heading':
+                    if ($value->children[0]->type == 'text') {
+                        $html_content .=
+                            '<h' .
+                            $value->level .
+                            '>' .
+                            RichTextParser::parseText($value->children[0]) .
+                            '</h' .
+                            $value->level .
+                            '>';
+                    } else {
+                        $html_content .= '<!-- ' . $value->type . ' is not implemented yet -->';
+                        // not implemented
+                    }
+                    break;
+
+                case 'list':
+                    $html_content .= RichTextParser::parseList($value);
+                    break;
+
+                case 'quote':
+                    $html_content .= '<blockquote>';
+                    foreach ($value->children as $key => $child) {
+                        if ($child->type == 'text') {
+                            $html_content .= '<p>' . RichTextParser::parseText($child) . '</p>';
+                        } else {
+                            $html_content .= '<!-- ' . $child->type . ' is not implemented yet -->';
+                            // not implemented
+                        }
+                    }
+                    $html_content .= '</blockquote>';
+                    break;
+
+                case 'code':
+                    $html_content .= '<pre><code>';
+                    $child = $value->children[0];
+                    if ($child->type == 'text') {
+                        $html_content .=  htmlspecialchars($child->text);
+                    } else {
+                        $html_content .= '<!-- ' . $child->type . ' is not implemented yet -->';
+                        // not implemented
+                    }
+                    $html_content .= '</code></pre>';
+                    break;
+
+                default:
+                    $html_content .= '<!-- ' . $value->type . ' is not implemented yet -->';
+                    // not implemented
+                    break;
+            }
+        }
+
+        return $html_content;
+    }
+}
---
+++ tests/CommonTest.php
@@ -0,0 +1,94 @@
+<?php
+
+require 'src/RichTextParser.php';
+
+use PHPUnit\Framework\TestCase;
+use HugoLevet\StrapiPhpRichTextParser\RichTextParser;
+
+final class CommonTest extends TestCase
+{
+    public function testHeading(): void
+    {
+        $this->assertEquals('<h1>Heading 1</h1>', RichTextParser::jsonToHtml(json_decode('[{"type":"heading","children":[{"type":"text","text":"Heading 1"}],"level": 1}]')));
+    }
+
+    public function testHeading2(): void
+    {
+        $this->assertEquals('<h2>Heading 2</h2>', RichTextParser::jsonToHtml(json_decode('[{"type":"heading","children":[{"type":"text","text":"Heading 2"}],"level":2}]')));
+    }
+
+    public function testParagraph(): void
+    {
+        $this->assertEquals('<p>Paragraph 1</p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"Paragraph 1"}]}]')));
+    }
+
+    public function testBold(): void
+    {
+        $this->assertEquals('<p><strong>Bold text</strong></p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"Bold text","bold":true}]}]')));
+    }
+
+    public function testItalic(): void
+    {
+        $this->assertEquals('<p><em>Italic text</em></p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"Italic text","italic":true}]}]')));
+    }
+
+    public function testUnderline(): void
+    {
+        $this->assertEquals('<p><u>Underline text</u></p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"Underline text","underline":true}]}]')));
+    }
+
+    public function testStrikethrough(): void
+    {
+        $this->assertEquals('<p><del>Strikethrough text</del></p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"Strikethrough text","strikethrough":true}]}]')));
+    }
+
+    public function testCode(): void
+    {
+        $this->assertEquals('<p><code>Code text</code></p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"Code text","code":true}]}]')));
+    }
+
+    public function testList(): void
+    {
+        $this->assertEquals('<ul><li>Item 1</li><li>Item 2</li></ul>', RichTextParser::jsonToHtml(json_decode('[{"type":"list","children":[{"type":"list-item","children":[{"type":"text","text":"Item 1"}]},{"type":"list-item","children":[{"type":"text","text":"Item 2"}]}],"format":"unordered"}]')));
+    }
+
+    public function testList2(): void
+    {
+        $this->assertEquals('<ol><li>Item 1</li><li>Item 2</li></ol>', RichTextParser::jsonToHtml(json_decode('[{"type":"list","children":[{"type":"list-item","children":[{"type":"text","text":"Item 1"}]},{"type":"list-item","children":[{"type":"text","text":"Item 2"}]}],"format":"ordered"}]')));
+    }
+
+    public function testList3(): void
+    {
+        $this->assertEquals('<ul><li>Item 1</li><li><ul><li>SubItem 1</li><li>SubItem 2</li></ul></li><li>Item 2</li></ul>', RichTextParser::jsonToHtml(json_decode('[{"type":"list","children":[{"type":"list-item","children":[{"type":"text","text":"Item 1"}]},{"type":"list","children":[{"type":"list-item","children":[{"type":"text","text":"SubItem 1"}]},{"type":"list-item","children":[{"type":"text","text":"SubItem 2"}]}],"format":"unordered"},{"type":"list-item","children":[{"type":"text","text":"Item 2"}]}],"format":"unordered"}]')));
+    }
+
+    public function testListWithBold(): void
+    {
+        $this->assertEquals('<ul><li>Part 1<strong> Bolded text</strong></li></ul>', RichTextParser::jsonToHtml(json_decode('[{"type":"list","children":[{"type":"list-item","children":[{"type":"text","text":"Part 1"},{"type":"text","text":" Bolded text","bold":true}]}],"format":"unordered"}]')));
+    }
+
+    public function testImage(): void
+    {
+        $this->assertEquals('<img src="http://localhost:1337/uploads/medium_placeholder.png" alt="Alternative text" width="750" height="375" loading="lazy" />', RichTextParser::jsonToHtml(json_decode('[{"type":"image","image":{"name":"Name image","alternativeText":"Alternative text","url":"http:\/\/localhost:1337\/uploads\/placeholder.png","width":1000,"height":500,"formats":{"thumbnail":{"name":"thumbnail_Name image","width":245,"height":122,"url":"\/uploads\/thumbnail_placeholder.png"},"small":{"name":"small_Name image","width":500,"height":250,"url":"\/uploads\/small_placeholder.png"},"medium":{"name":"medium_Name image","width":750,"height":375,"url":"\/uploads\/medium_placeholder.png"},"large":{"name":"large_Name image","width":1000,"height":500,"url":"\/uploads\/large_placeholder.png"}}}}]')));
+    }
+
+    public function testLinkInParagraph(): void
+    {
+        $this->assertEquals('<p>para<a href="https://example.com" target="_blank" rel="noopener noreferrer">Example</a>graph</p>', RichTextParser::jsonToHtml(json_decode('[{"type":"paragraph","children":[{"type":"text","text":"para"},{"type":"link","url":"https://example.com","children":[{"type":"text","text":"Example"}]},{"type":"text","text":"graph"}]}]')));
+    }
+
+    public function testQuote(): void
+    {
+        $this->assertEquals('<blockquote><p>Quote</p></blockquote>', RichTextParser::jsonToHtml(json_decode('[{"type":"quote","children":[{"type":"text","text":"Quote"}]}]')));
+    }
+
+    public function testCodeBlock(): void
+    {
+        $this->assertEquals('<pre><code>Code block</code></pre>', RichTextParser::jsonToHtml(json_decode('[{"type":"code","children":[{"type":"text","text":"Code block"}]}]')));
+    }
+
+    public function testListWithLink(): void
+    {
+        $this->assertEquals('<ul><li>Item 1</li><li>Item 2 <a href="https://hugolevet.fr/" target="_blank" rel="noopener noreferrer">with link</a></li></ul>', RichTextParser::jsonToHtml(json_decode('[{"type":"list","children":[{"type":"list-item","children":[{"type":"text","text":"Item 1"}]},{"type":"list-item","children":[{"type":"text","text":"Item 2 "},{"type":"link","url":"https:\/\/hugolevet.fr\/","children":[{"type":"text","text":"with link"}]},{"text":"","type":"text"}]}],"format":"unordered"}]')));
+    }
+}
